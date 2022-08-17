150a4535e6db4eef581091ed5896a92357146629
Nijat Muhtar <nijatmn@gmail.com>
2022-08-17 19:37:24 -0400

    modified CreatePage class

--- src/test/java/pages/CreatePage.java
+++ src/test/java/pages/CreatePage.java
@@ -1,4 +1,8 @@
 package pages;
 
 public class CreatePage {
+    public static void main(String[] args) {
+        System.out.println("Hello guys!");
+        System.out.println("This code is from master");
+    }
 }
